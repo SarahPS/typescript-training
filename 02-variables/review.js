361bc27738d141b8c2c815ead1c1b7cf5f373c64
SarahPS <mail@sarahperrystout.com>
2026-07-13 17:40:41 -0400

    loops

--- 02-variables/review.js
+++ 02-variables/review.js
@@ -15,3 +15,27 @@ for (var i = 0; i < numbers.length; i++) {
 }
 var average = sum / numbers.length;
 console.log(average);
+//loop to get access to each object in an array held by a temporary variable
+var sportsOne = ['soccer', 'basketball', 'baseball', 'football'];
+for (var _i = 0, sportsOne_1 = sportsOne; _i < sportsOne_1.length; _i++) {
+    var tempSport = sportsOne_1[_i];
+    console.log(tempSport);
+}
+//loop to get first letter of each object in an array held by a temporary variable
+for (var _a = 0, sportsOne_2 = sportsOne; _a < sportsOne_2.length; _a++) {
+    var tempSport = sportsOne_2[_a];
+    console.log(tempSport.charAt(0));
+}
+// add more  elements to the array   
+sportsOne.push("cricket");
+sportsOne.push("golf", "tennis");
+// add a conditional to the loop  
+for (var _b = 0, sportsOne_3 = sportsOne; _b < sportsOne_3.length; _b++) {
+    var tempSport = sportsOne_3[_b];
+    if (tempSport == "football") {
+        console.log("I love football!");
+    }
+    else {
+        console.log(tempSport);
+    }
+}
