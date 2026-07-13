d2616c2b0e9105d5d67335b917ee0e13b6d67144
SarahPS <mail@sarahperrystout.com>
2026-07-13 16:14:51 -0400

    updates to typeSCript varaibles and output

--- 01-helloworld/myhelloworld.js
+++ 01-helloworld/myhelloworld.js
@@ -1,2 +1,10 @@
 console.log("Hello World");
-console.log("Hey Coding Gurus! " + "Tme for some work");
+console.log("Hey Coding Gurus! " + "Time for some work");
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = "Kumar";
+console.log(found);
+console.log("The grade is: " + grade);
+//console.log("Hi " + firstName + " " + lastName);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
